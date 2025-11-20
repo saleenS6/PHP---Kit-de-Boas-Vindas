54580e07e9b25160bcb4baa75a00d7e19148aba5
Pedro Vitor <pedroEIC_olivera@hotmail.com>
2025-11-20 14:48:37 -0300

    explicação de como fazer um Hello World em php refs #1

---
+++ WelloWorld/teste.php
@@ -0,0 +1,13 @@
+<?php
+    // A tag <?php abre o bloco onde podemos escrever código PHP.
+    // Tudo aqui dentro será interpretado pelo servidor.
+
+    /*
+        O comando echo envia texto para o navegador.
+        Ele é usado para exibir mensagens, valores, variáveis etc.
+        No caso abaixo, ele vai mostrar "Hello World!" na tela.
+    */
+    echo "Hello World!";
+
+    // A tagfecharia o bloco PHP, porém ela é opcional.
+?>
